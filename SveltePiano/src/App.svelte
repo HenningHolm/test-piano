36e7a4867af46e7c91b9c2f23239c4f73a2cfe3f
henningholm <henningholm90@gmail.com>
2025-06-06 14:18:09 +0200

    Mini cleanup

--- SveltePiano/src/App.svelte
+++ SveltePiano/src/App.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
   import ThePanel from './components/Panel/ThePanel.svelte'
   import TheKeyboard from './components/Keyboard/TheKeyboard.svelte'
   import TheSheet from './components/Sheet/TheSheet.svelte'
